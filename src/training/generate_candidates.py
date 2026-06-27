27e764a5f14f7df4f61a0ff221b67c505e028e20
Dira Amanda <66349501+amandasbrn@users.noreply.github.com>
2026-06-27 18:56:21 +1000

    Smoke test one jsonl line of generating candidate answer

--- src/training/generate_candidates.py
+++ src/training/generate_candidates.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from pathlib import Path
+import json
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from src.data.prompt_templates import build_study_friendly_prompt, build_formal_prompt
 
 MODEL_NAME = "Qwen/Qwen2.5-0.5B-Instruct"
 PROMPT_PATH = Path("data/raw/study_prompts.csv")
+OUTPUT_PATH = Path("data/processed/candidate_answers.jsonl")
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
@@ -15,8 +17,7 @@ def load_prompt(data: Path) -> pd.DataFrame:
     df = pd.read_csv(data)
     return df
 
-def format_prompt(data: pd.DataFrame) -> str:
-    question = data['prompt'].iloc[0]
+def format_prompt(question: str) -> str:
     prompt_a = build_study_friendly_prompt(question)
     prompt_b = build_formal_prompt(question)
     return prompt_a, prompt_b
@@ -39,7 +40,7 @@ def generate_output(formatted_prompt: str):
     )
 
     inputs = tokenizer([text], return_tensors="pt")
-    answer_a = model.generate(
+    answer = model.generate(
             **inputs,
             max_new_tokens=300,
             do_sample=False,
@@ -48,20 +49,36 @@ def generate_output(formatted_prompt: str):
         )
     
     input_length = inputs["input_ids"].shape[1]
-    generated_tokens = answer_a[0][input_length:]
+    generated_tokens = answer[0][input_length:]
     decoded = tokenizer.decode(generated_tokens, skip_special_tokens=True)
     return decoded
 
 def main():
     prompt = load_prompt(PROMPT_PATH)
-    prompt_a, prompt_b = format_prompt(prompt)
+    question = prompt.iloc[0]
+    prompt_a, prompt_b = format_prompt(question)
     output_a = generate_output(prompt_a)
     output_b = generate_output(prompt_b)
-    print("STUDY FRIENDLY")
-    print(output_a)
-    print('-------------------------------------')
-    print("FORMAL")
-    print(output_b)
+
+    record = {
+        'id': question['id'],
+        'topic': question['topic'],
+        'subtopic': question['subtopic'],
+        'prompt': question['prompt'],
+        'answer_a': output_a,
+        'answer_b': output_b
+    }
+
+    with open(OUTPUT_PATH, "w", encoding="utf-8") as f:
+        f.write(json.dumps(record, ensure_ascii=False) + "\n")
+
+    print('Successfully saved candidate_answers.jsonl file!')
+
+    # print("STUDY FRIENDLY")
+    # print(output_a)
+    # print('-------------------------------------')
+    # print("FORMAL")
+    # print(output_b)
 
 if __name__ == "__main__":
     main()
