0dd28f9cb135e8e41f25e9c371ead12b5ac67e79
Dira Amanda <66349501+amandasbrn@users.noreply.github.com>
2026-07-15 15:27:04 +1000

    Adding sys path for streamlit deployment

--- src/app/app.py
+++ src/app/app.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+sys.path.append(str(PROJECT_ROOT))
+
 import streamlit as st
 from src.data.prompt_templates import build_study_friendly_prompt
 from transformers import AutoModelForCausalLM, AutoTokenizer
